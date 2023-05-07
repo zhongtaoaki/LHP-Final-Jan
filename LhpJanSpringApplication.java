130eda2773b21717fb5bbfd1e5fe5985d6f72cfe
zhongtaoaki <49824985+zhongtaoaki@users.noreply.github.com>
2023-05-07 18:55:28 +0900

    Add files via upload

---
+++ LhpJanSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhpJanSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhpJanSpringApplication.class, args);
+	}
+
+}
